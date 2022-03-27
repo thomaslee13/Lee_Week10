4d296f0f36b360337848e76aca296846afcb88c9
thomaslee13 <ev5130ke@go.minnstate.edu>
2022-03-27 11:34:04 -0500

    Add project files.

---
+++ Lee_Week10/Controllers/PurchaseOrderDetailsController.cs
@@ -0,0 +1,123 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Lee_Week10.Data;
+using Lee_Week10.Models;
+
+namespace Lee_Week10.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PurchaseOrderDetailsController : ControllerBase
+    {
+        private readonly Adventureworks2019Context _context;
+
+        public PurchaseOrderDetailsController(Adventureworks2019Context context)
+        {
+            _context = context;
+        }
+
+        // GET: api/PurchaseOrderDetails
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<PurchaseOrderDetail>>> GetPurchaseOrderDetails()
+        {
+            return await _context.PurchaseOrderDetails.ToListAsync();
+        }
+
+        // GET: api/PurchaseOrderDetails/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<PurchaseOrderDetail>> GetPurchaseOrderDetail(int id)
+        {
+            var purchaseOrderDetail = await _context.PurchaseOrderDetails.FindAsync(id);
+
+            if (purchaseOrderDetail == null)
+            {
+                return NotFound();
+            }
+
+            return purchaseOrderDetail;
+        }
+
+        // PUT: api/PurchaseOrderDetails/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPurchaseOrderDetail(int id, PurchaseOrderDetail purchaseOrderDetail)
+        {
+            if (id != purchaseOrderDetail.PurchaseOrderId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(purchaseOrderDetail).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PurchaseOrderDetailExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/PurchaseOrderDetails
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<PurchaseOrderDetail>> PostPurchaseOrderDetail(PurchaseOrderDetail purchaseOrderDetail)
+        {
+            _context.PurchaseOrderDetails.Add(purchaseOrderDetail);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (PurchaseOrderDetailExists(purchaseOrderDetail.PurchaseOrderId))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetPurchaseOrderDetail", new { id = purchaseOrderDetail.PurchaseOrderId }, purchaseOrderDetail);
+        }
+
+        // DELETE: api/PurchaseOrderDetails/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePurchaseOrderDetail(int id)
+        {
+            var purchaseOrderDetail = await _context.PurchaseOrderDetails.FindAsync(id);
+            if (purchaseOrderDetail == null)
+            {
+                return NotFound();
+            }
+
+            _context.PurchaseOrderDetails.Remove(purchaseOrderDetail);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PurchaseOrderDetailExists(int id)
+        {
+            return _context.PurchaseOrderDetails.Any(e => e.PurchaseOrderId == id);
+        }
+    }
+}
---
+++ Lee_Week10/Controllers/WorkOrdersController.cs
@@ -0,0 +1,109 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Lee_Week10.Data;
+using Lee_Week10.Models;
+
+namespace Lee_Week10.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WorkOrdersController : ControllerBase
+    {
+        private readonly Adventureworks2019Context _context;
+
+        public WorkOrdersController(Adventureworks2019Context context)
+        {
+            _context = context;
+        }
+
+        // GET: api/WorkOrders
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<WorkOrder>>> GetWorkOrders()
+        {
+            return await _context.WorkOrders.ToListAsync();
+        }
+
+        // GET: api/WorkOrders/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<WorkOrder>> GetWorkOrder(int id)
+        {
+            var workOrder = await _context.WorkOrders.FindAsync(id);
+
+            if (workOrder == null)
+            {
+                return NotFound();
+            }
+
+            return workOrder;
+        }
+
+        // PUT: api/WorkOrders/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutWorkOrder(int id, WorkOrder workOrder)
+        {
+            if (id != workOrder.WorkOrderId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(workOrder).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!WorkOrderExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/WorkOrders
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<WorkOrder>> PostWorkOrder(WorkOrder workOrder)
+        {
+            _context.WorkOrders.Add(workOrder);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetWorkOrder", new { id = workOrder.WorkOrderId }, workOrder);
+        }
+
+        // DELETE: api/WorkOrders/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteWorkOrder(int id)
+        {
+            var workOrder = await _context.WorkOrders.FindAsync(id);
+            if (workOrder == null)
+            {
+                return NotFound();
+            }
+
+            _context.WorkOrders.Remove(workOrder);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool WorkOrderExists(int id)
+        {
+            return _context.WorkOrders.Any(e => e.WorkOrderId == id);
+        }
+    }
+}
